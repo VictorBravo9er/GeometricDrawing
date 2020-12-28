d4fc0b69198d1f2979bbf4c756512c680edcf751
VictorBravo9er <vikramjitborah@gmail.com>
2020-12-28 10:09:28 +0530

    rules

--- xtraTests/rule.py
+++ xtraTests/rule.py
@@ -2,5 +2,5 @@ import __init__
 
 from Parser.parse import Parser, Collector
 
-with open("rule_no_inheritance.txt", "w") as file:
+with open("rule.txt", "w") as file:
     file.write(Parser.help())
