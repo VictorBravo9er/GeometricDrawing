503a9b9e3ace9c575c2e4e8c14834dcad84abaaa
VictorBravo9er <vikramjitborah@gmail.com>
2021-02-27 19:26:40 +0530

    q

--- app.py
+++ app.py
@@ -4,16 +4,21 @@ from figures.base import base
 from Drawables.Drawable import Drawable as dw
 import streamlit as st
 
+
 @st.cache(persist=True, allow_output_mutation=True)
-def getParser(input:int):
+def getParser(input):
     """Get Parser."""
     return base.modify(input)
 
+
 st.title("Number")
-inp: int = st.number_input("Input a Number", min_value=0, max_value=(1<<53-1), value=0, step=1)
+inp: int = st.number_input(
+    "Input a Number", min_value=0,
+    max_value=(1 << 53 - 1),
+    value=0, step=1
+)
 
 
 if st.button("Go"):
     items = base.modify(inp)
     a = st.pyplot(dw.draw(items, _show=False), clear_figure=False)
-    
